c88a70793cb91e46aff2b39b3f0b6e84aba8e4da
rbcribeiro <rbcribeiro@gmail.com>
2024-05-31 18:28:32 -0300

    feat: Criando busca por nome

--- src/Components/ItemLista/ItemLista.jsx
+++ src/Components/ItemLista/ItemLista.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaEdit } from "react-icons/fa";
 import "./ItemLista.css";
 
 const ItemLista = ({ items, abrirModalEdicao }) => {
+  const [termoBusca, setTermoBusca] = useState("");
+
   const handleEditarClick = (item) => {
     abrirModalEdicao(item);
   };
 
+  const handleTermoBuscaChange = (event) => {
+    setTermoBusca(event.target.value);
+  };
+
+  const filteredItems = items.filter(item =>
+    item.username.toLowerCase().startsWith(termoBusca.toLowerCase())
+  );
+
   return (
     <div className="item-list-container">
       <h2>Cadastrados</h2>
+      <input
+        type="text"
+        placeholder="Buscar por nome..."
+        value={termoBusca}
+        onChange={handleTermoBuscaChange}
+      />
       <ul>
-        {items.map((item, index) => (
+        {filteredItems.map((item, index) => (
           <li key={index} className="list-item">
             <p>Nome: {item.username}</p>
             <p>Endereço: {item.address}</p>
